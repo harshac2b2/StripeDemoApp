3749f0adecf2491f1f63e9b9065a464cf3b03062
harshac2b2 <65428901+harshac2b2@users.noreply.github.com>
2021-02-19 19:22:18 -0800

    Add files via upload

---
+++ StripeChargeAPI/StripeChargeAPI/Controllers/ProductsController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace StripeChargeAPI.Controllers
+{
+    //[Route("api/stripe/products")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [Route("api/stripe/products")]
+        [HttpGet]
+        public JsonResult Products()
+        {           
+
+            var productsList = new List<Product>()
+            {
+              new Product("XRYGEF","Ring Video Door Bell",55.99,"https://cdn.shopify.com/s/files/1/2393/8647/products/ring_video_doorbell_wired_1000x1000_099f9c9a-fe6b-4ff5-9543-0757a5a7b5bf.jpg?v=1611611506"),
+              new Product("XYZDJF","Google Nest Hello - A better door Bell",159.99,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzcsgVaUKICpvogj-JtY4z4WIai2ngzusJ35U0tTytA522x7rEiSiMxvr1xQbU4DRYyEv0hhg&usqp=CAc"),
+              //new Product("MRGDGW","eufy Security Video Door Bell",99.99,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVWtrt6EG2tfR8asJeY_CkXxZ5In2AEO_Gnq9oMpv_3ud9t43kFWJO1WxNiYUUYXnVa7Sz-IX9&usqp=CAc"),
+              //new Product("XPRDOP","Arlo Video Doorbell",119.99,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8-5cc4pbF6DfSaQMG8VMqpe1orxkbRQo9joSR_fPuY2CwPgcVzOq5sYD5j50VP5PdllOHFa0j&usqp=CAc")
+
+            };            
+            
+            return new JsonResult(productsList);
+        }
+
+        // GET api/<ProductsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }        
+        
+    }
+
+    public class Product
+    {
+        public Product(string _product_code, string _product_name, double _product_cost, string _product_image)
+        {
+            product_code = _product_code;
+            product_name = _product_name;
+            product_cost = _product_cost;
+            product_image = _product_image;
+        }
+       
+        public string product_code { get; set; }
+        public string product_name { get; set; }
+        public double product_cost { get; set; }
+        public string product_image { get; set; }
+
+    }
+}
---
+++ StripeChargeAPI/StripeChargeAPI/Controllers/StripeCreateChargeController.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Stripe;
+using Microsoft.Extensions.Configuration;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace StripeChargeAPI.Controllers
+{
+    [ApiController]
+    //[Route("api/stripe/charge")]    
+    public class StripeCreateChargeController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public StripeCreateChargeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [Route("api/stripe/test")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "test1", "test2" };
+        }
+
+        [Route("api/stripe/charge")]
+        [HttpPost] 
+        public JsonResult Charge([FromBody]ChargeRequest request)
+        {
+            string apiKey = _configuration.GetSection("StripeApiKey").Value;
+            if(string.IsNullOrEmpty(apiKey))
+            {
+                throw new Exception("Stripe Api Key is invalid/ missing from the API Configuration");
+            }
+
+            StripeConfiguration.ApiKey = apiKey;            
+
+            ChargeShippingOptions shippingOptions = new ChargeShippingOptions();
+            AddressOptions addressOptions = new AddressOptions();
+            ChargeCreateOptions options = new ChargeCreateOptions();
+            ChargeResponse response = new ChargeResponse();         
+
+
+            try
+            {
+                options.Amount = request.amount;
+                options.Currency = request.currency;
+                options.Source = "tok_visa";
+                options.Description = request.description;
+                options.ReceiptEmail = request.receiptEmail;
+                addressOptions.City = request.shipping.Address.City;
+                addressOptions.State = request.shipping.Address.State;
+                addressOptions.Line1 = request.shipping.Address.Line1;
+                addressOptions.PostalCode = request.shipping.Address.PostalCode;
+                addressOptions.Country = request.shipping.Address.Country;
+                shippingOptions.Address = addressOptions;
+                shippingOptions.Name = request.shipping.Name;         
+                
+
+                shippingOptions.Address = addressOptions;
+                options.Shipping = shippingOptions;
+
+                var service = new ChargeService();
+                Charge chargeResponse = service.Create(options);
+
+
+                if(chargeResponse == null)
+                {
+                    throw new Exception("Payment not successfull! Call customer support for more details");                   
+                }
+
+                response.response_id = chargeResponse.Id;
+                double amountCharged = (double)chargeResponse.AmountCaptured/100;
+                response.amount_charged = "$"+amountCharged.ToString();
+                response.status = "Success";
+                return new JsonResult(response);
+
+            }
+            catch(Exception ex)
+            {
+                response.error = ex.Message;
+                response.status = "ERROR";
+                return new JsonResult(response);
+            }
+
+
+
+        }        
+    }
+
+    public class ChargeRequest
+    {
+
+        public long amount { get; set; }
+        public string currency { get; set; }
+        public string description { get; set; }
+        //public string source { get; set; }
+        public string receiptEmail { get; set; }
+        public Shipping shipping { get; set; }
+        public string name { get; set; }
+    }
+
+    public class ChargeResponse
+    {
+        public string response_id { get; set; }
+        public string amount_charged { get; set; }
+        public string status { get; set; }
+        public string error { get; set; }
+    }
+}
